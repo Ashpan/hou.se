c9a59e813ebe4ff61c8d0b2e4f168552718dfdf6
Matt Lemcke <53319315+Matt-Lemcke@users.noreply.github.com>
2020-01-25 13:21:12 -0500

    More pages

--- houseapp/urls.py
+++ houseapp/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('house/', views.house, name='house'),
+    path('tasks/', views.tasks, name='tasks'),
+    path('calendar/', views.calendar, name='calendar'),
 ]
--- houseapp/views.py
+++ houseapp/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'houseapp/home.html')
+
+
+def house(request):
+    return render(request, 'houseapp/house.html')
+
+
+def tasks(request):
+    return render(request, 'houseapp/tasks.html')
+
+
+def calendar(request):
+    return render(request, 'houseapp/calendar.html')
