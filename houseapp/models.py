bdad3c19e35326181f7a0f129db94ee6e5d5352a
Haxerus <haxerus.corporation@gmail.com>
2020-01-25 12:51:42 -0500

    Added house model

--- houseapp/models.py
+++ houseapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class House(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    invite_code = models.CharField(max_length=6)
+    members = models.ForeignKey(User, on_delete=models.CASCADE)
+
