d75545c14580b2fb3d0541d51d752e9b555a419d
Haxerus <haxerus.corporation@gmail.com>
2020-01-25 18:07:25 -0500

    Task database stuff

--- houseapp/models.py
+++ houseapp/models.py
@@ -3,6 +3,7 @@ from datetime import date
 from django.db import models
 from django.http import HttpResponse
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class House(models.Model):
@@ -17,5 +18,8 @@ class Task(models.Model):
     due_date = models.DateField(default=date.today())
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
 
 
--- houseapp/views.py
+++ houseapp/views.py
@@ -1,17 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 
 from .models import Task
-
-
-def home(request):
-    context = {
-        'tasks': Task.objects.all()
-    }
-
-    return render(request, 'houseapp/home.html')
-
 
 def house(request):
     return render(request, 'houseapp/house.html')
@@ -29,3 +20,13 @@ class TaskListView(ListView):
     model = Task
     template_name = 'houseapp/home.html'
     context_object_name = 'tasks'
+
+class TaskCreateView(CreateView):
+    model = Task
+    fields = ['title', 'due_date', 'user']
+    success_url = '/'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
