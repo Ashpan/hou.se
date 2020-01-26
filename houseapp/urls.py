7e44da606a5a1f0f4d2b0842a5c91541e81c123f
Haxerus <haxerus.corporation@gmail.com>
2020-01-26 01:40:23 -0500

    I am really upset that this took so long

--- houseapp/urls.py
+++ houseapp/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from .views import TaskListView, TaskCreateView, TaskDeleteView
+from .views import TaskListView, TaskCreateView, TaskDeleteView, TaskCompleteView
 from . import views
 
 urlpatterns = [
     path('', TaskListView.as_view(), name='home'),
     path('house/', views.house, name='house'),
     path('tasks/', views.tasks, name='tasks'),
+    path('tasks/<int:pk>/complete/', TaskCompleteView.as_view(), name='task-complete'),
     path('tasks/new/', TaskCreateView.as_view(), name='task-create'),
     path('calendar/', views.calendar, name='calendar'),
-    path('tasks/<int:pk>/delete', TaskDeleteView.as_view(), name="task-delete")
+    path('tasks/<int:pk>/delete/', TaskDeleteView.as_view(), name="task-delete")
 ]
