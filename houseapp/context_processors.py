45c0270c07c0285d4aa4cd315ac1cc8ecae860d1
Haxerus <haxerus.corporation@gmail.com>
2020-01-26 06:52:29 -0500

    Fixed context processor

--- houseapp/context_processors.py
+++ houseapp/context_processors.py
@@ -2,6 +2,13 @@ from houseapp.models import Membership
 
 
 def add_variable_to_context(request):
+    name = 'House Name'
+
+    print(request.user)
+
+    if request.user.is_authenticated:
+        name = Membership.objects.get(person=request.user).house
+
     return {
-        'housename': Membership.objects.get(person=request.user).house
+        'housename': name
     }
