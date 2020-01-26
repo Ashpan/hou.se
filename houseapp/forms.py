67ba3ebfcb3a1a50588bede6716018f61e0b4be5
Matt Lemcke <53319315+Matt-Lemcke@users.noreply.github.com>
2020-01-25 23:58:24 -0500

    Create house form made

--- houseapp/forms.py
+++ houseapp/forms.py
@@ -2,7 +2,11 @@ from django import forms
 from .models import House
 
 
+<<<<<<< Updated upstream
 class HouseRegisterForm(forms.ModelForm):
+=======
+class HouseRegisterForm(ModelForm):
+>>>>>>> Stashed changes
     class Meta:
         model = House
         fields = ['name', 'address']
